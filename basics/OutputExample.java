81ebff926f839308cf4d920cedbd9d8721ff92a6
SaiyamHub <saiyamkumar08@gmail.com>
2026-01-21 16:17:19 +0530

    Update OutputExample.java

--- basics/OutputExample.java
+++ basics/OutputExample.java
@@ -1,1 +1,23 @@
+public class OutputExample {
+    public static void main(String[] args) {
+
+        // println -> prints and moves to next line
+        System.out.println("Java Programming Language");
+        System.out.println("Java is OOP Language");
+        System.out.println("Java is Portable Language");
+
+        // print -> same line
+        System.out.print("Hello ");
+        System.out.println("World");
+
+        // printing numbers
+        System.out.println(33);
+        System.out.println(3345);
+        System.out.println(56798);
+
+        // operations
+        System.out.println(5 + 7);
+        System.out.println(7 * 5);
+    }
+}
 
