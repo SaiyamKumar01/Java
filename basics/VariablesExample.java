a770cfd135df2c44e48de032abab04741c12b6bb
SaiyamHub <saiyamkumar08@gmail.com>
2026-01-21 16:19:31 +0530

    Update VariablesExample.java

--- basics/VariablesExample.java
+++ basics/VariablesExample.java
@@ -1,1 +1,20 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+
+        int num = 15;
+        System.out.println(num);
+
+        String name = "Saiyam";
+        System.out.println(name);
+
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun);
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        float number = 5.9f;
+        System.out.println(number);
+    }
+}
 
