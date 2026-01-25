c8922ede92c2e0f9ee2150bef5e220a6ebef374b
SaiyamKumar01 <saiyamkumar08@gmail.com>
2026-01-25 22:04:29 +0530

    Add Inheritance

--- 3) Classes/EPackagesInput.java
+++ 3) Classes/EPackagesInput.java
@@ -1,11 +1,10 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Scanner;
 public class EPackagesInput{
-
 //Used to group related classes
 //1)Built-in Packages -> import java.util.*; (for all packages)
-                //    -> import java.time
+                //    -> import java.time -
                /*LocalDate 	Represents a date (year, month, day (yyyy-MM-dd))
                 LocalTime 	Represents a time (hour, minute, second and nanoseconds (HH-mm-ss-ns))
                 LocalDateTime 	Represents both a date and a time (yyyy-MM-dd-HH-mm-ss-ns)
@@ -13,7 +12,7 @@ public class EPackagesInput{
 
 //2)User defined Packages -> used in multiple classes
 
-/* 
+/*                                                    
 nextBoolean() -> Reads a boolean value from the user
 nextByte() -> Reads a byte value from the user
 nextDouble() -> Reads a double value from the user
@@ -23,7 +22,7 @@ nextLine() -> Reads a String value from the user
 nextLong() -> Reads a long value from the user
 nextShort() -> Reads a short value from the user
 */
-
+             
     public static void main(String args[]){
         Scanner obj = new Scanner(System.in);
 
---
+++ 3) Classes/FInheritance.java
@@ -0,0 +1,24 @@
+// Inheritance means inheriting attributes and methods from another class
+public class FInheritance {
+
+    // Parent class => (Super-class)
+    static class Vehicle {
+        protected String model = "Ford";
+
+        public void horn() {
+            System.out.println("honk honk");
+        }
+    }
+
+    // Child class => (Sub-class)
+    static class Car extends Vehicle { //used extend keyword
+        private String carname = "Mustang";
+
+        public static void main(String[] args) {
+            Car mycar = new Car();
+
+            mycar.horn();
+            System.out.println(mycar.carname + " " + mycar.model);
+        }
+    }
+}
